e530412c51acea24a01d60be920cb9e86eb0da4f
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-25 15:45:25 -0400

    feat: new component grid final

---
+++ src/components/GridListVideos/index.tsx
@@ -0,0 +1,7 @@
+import FileManager from '@/services/useFileManager';
+import BoxVideo from './BoxVideo';
+import { StyledGridList } from './StyledGridList';
+
+export default function GridList() {
+    return <StyledGridList></StyledGridList>;
+}
