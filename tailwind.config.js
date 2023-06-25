e3e66b0a4cfaafd782ed8e40d4a723884a07ba56
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-25 13:52:52 -0400

    first commit: add files and configurations

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const { background } = require('@chakra-ui/react');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                white: '#fff',
+                sectionBackground: '#f0f8ff',
+                primaryBlue: '#007DF9',
+                textColor1: '#1c3c50',
+                textColor2: '#0084ff',
+            },
+
+            fontFamily: { jakarta: ['Plus Jakarta Sans', 'sans-serif'] },
+        },
+    },
+    plugins: [],
+};
