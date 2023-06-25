acbc20bd9b1e67c06b1262713b5f38ebc64bc309
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-25 15:45:11 -0400

    feat: new styled grid base

---
+++ src/components/GridListVideos/StyledGridList.ts
@@ -0,0 +1,9 @@
+'use client';
+import styled from 'styled-components';
+
+export const StyledGridList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+    place-items: center;
+`;
