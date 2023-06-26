8fcab37f9d33c3d159feb45a4f6bbe56948ac290
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 03:17:44 -0400

    fix: add logical rules in gridList

--- src/components/GridListVideos/index.tsx
+++ src/components/GridListVideos/index.tsx
@@ -1,7 +1,40 @@
-import FileManager from '@/services/useFileManager';
+import { useState } from 'react';
+import ModalBase from '../ModalBase';
 import BoxVideo from './BoxVideo';
 import { StyledGridList } from './StyledGridList';
+import { IVideos } from '@/interface/IVideos';
 
-export default function GridList() {
-    return <StyledGridList></StyledGridList>;
+interface IGridListProps {
+    currentData: any;
+}
+export default function GridList({ currentData }: IGridListProps) {
+    const [isOpen, setIsOpen] = useState(true);
+    const [currentVideo, setCurrentVideo] = useState<IVideos>({
+        category: 'teste',
+        date: '202323',
+        link: 'aslakjslkas',
+        title: 'testefeito',
+    });
+    return (
+        <StyledGridList>
+            {currentData.map((item: any) => (
+                <BoxVideo
+                    id={item.id}
+                    key={item.id}
+                    title={item.title}
+                    onClick={() => {
+                        setCurrentVideo(item);
+                        setIsOpen(!isOpen);
+                    }}
+                />
+            ))}
+            <ModalBase
+                isModalOpen={isOpen}
+                callback={() => {
+                    setIsOpen(!isOpen);
+                }}
+                model={currentVideo}
+            />
+        </StyledGridList>
+    );
 }
