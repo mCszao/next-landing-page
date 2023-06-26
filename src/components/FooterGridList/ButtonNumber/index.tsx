f30cb390bd75e598b0942194241edc75d386396e
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 05:41:39 -0400

    feat: new callback function to buttons pagination

--- src/components/FooterGridList/ButtonNumber/index.tsx
+++ src/components/FooterGridList/ButtonNumber/index.tsx
@@ -1,9 +1,16 @@
 export interface IButtonNumberProps {
     pageNumber: string | number;
+    callback: () => void;
 }
-export default function ButtonNumber({ pageNumber }: IButtonNumberProps) {
+export default function ButtonNumber({
+    pageNumber,
+    callback,
+}: IButtonNumberProps) {
     return (
-        <button className='rounded-lg  p-1 px-3 border border-textColor1 hover:border-primaryBlue hover:text-primaryBlue font-semibold focus:text-white focus:bg-primaryBlue focus:border-primaryBlue'>
+        <button
+            className='rounded-lg  p-1 px-3 border border-textColor1 hover:border-primaryBlue hover:text-primaryBlue font-semibold focus:text-white focus:bg-primaryBlue focus:border-primaryBlue'
+            onClick={() => callback()}
+        >
             {pageNumber}
         </button>
     );
