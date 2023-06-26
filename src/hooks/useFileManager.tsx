539a5cd5bfb221396643eefa633a055d19f0bbac
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 05:41:10 -0400

    feat: new controll pagination

--- src/hooks/useFileManager.tsx
+++ src/hooks/useFileManager.tsx
@@ -4,17 +4,23 @@ import { IVideos } from '@/interface/IVideos';
 
 export const useFileManager = () => {
     const [category, setCategory] = useState('agency');
-
+    const [currentPage, setCurrentPage] = useState(0);
     const dataJSON = Data.videos;
     const filteredList = filteredData(dataJSON, category!);
+    const pages = Math.ceil(filteredList.length / 9);
+    const starIndex = currentPage * 9;
+    const endIndex = starIndex + 9;
 
     const setFilter = (category: string) => {
         setCategory(category);
     };
     return {
-        currentData: filteredList,
+        currentData: filteredList.slice(starIndex, endIndex),
         currentCategory: category,
-        setFilter
+        setFilter,
+        currentPage,
+        setCurrentPage,
+        totalPages: pages,
     };
 };
 
