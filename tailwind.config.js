c8f212abb26f872b9365fe3fa7d2b7724c019677
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 04:24:29 -0400

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
                 primaryBlue: '#007DF9',
                 textColor1: '#1c3c50',
                 textColor2: '#0084ff',
+                buttonGreen: '#9ceddd',
+                buttonBlue: '#a1d9ff',
+                buttonYeallow: '#fff1a0',
             },
 
             fontFamily: { jakarta: ['Plus Jakarta Sans', 'sans-serif'] },
