fb2810f5b94d79cbc6e25b5fb8faffaa6cbdcb41
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 03:16:02 -0400

    fix: add call function to box component

--- src/components/GridListVideos/BoxVideo/index.tsx
+++ src/components/GridListVideos/BoxVideo/index.tsx
@@ -1,22 +1,31 @@
 import Image, { StaticImageData } from 'next/image';
 import Thumb from '../../../assets/images/thumbnail.png';
+import { StyledImageContainer } from './StyledImageContainer';
+import { FaPlay } from 'react-icons/fa';
 export interface IBoxVideoProps {
-    id: number;
+    id?: number;
     image?: StaticImageData;
     title: string;
+    onClick: () => void;
 }
-export default function BoxVideo({ id, title }: IBoxVideoProps) {
+export default function BoxVideo({ id, title, onClick }: IBoxVideoProps) {
     return (
         <article
             key={id}
             style={{ boxShadow: '5px 10px 15px rgba(0, 0, 0, 0.123)' }}
-            className=' rounded-lg w-3/4 relative flex flex-col justify-center box-border hover:cursor-pointer transition '
+            className=' rounded-lg w-72 relative flex flex-col justify-center box-border hover:cursor-pointer transition hover:text-primaryBlue font-semibold'
+            onClick={() => onClick()}
         >
-            <Image
-                src={Thumb}
-                alt='Thumbnail do vídeo'
-                className='rounded-t-lg'
-            />
+            <StyledImageContainer>
+                <div>
+                    <Image
+                        src={Thumb}
+                        alt='Thumbnail do vídeo'
+                        className='rounded-t-lg '
+                    />
+                </div>
+                <FaPlay color='#0084ff' size={'3em'} className='icon' />
+            </StyledImageContainer>
             <h3 className='m-4'>{title}</h3>
         </article>
     );
