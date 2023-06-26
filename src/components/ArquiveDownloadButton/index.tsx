91fad4a0995511b1a584b4c3e9320051033f6834
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 04:39:31 -0400

    feat: new semantical prop

--- src/components/ArquiveDownloadButton/index.tsx
+++ src/components/ArquiveDownloadButton/index.tsx
@@ -2,13 +2,16 @@ import { GiCloudDownload } from 'react-icons/gi';
 interface IArquiveDownloadButtonProps {
     text: string;
     color?: string;
+    description: string;
 }
 export default function ArquiveDownloadButton({
     text,
     color,
+    description,
 }: IArquiveDownloadButtonProps) {
     return (
         <button
+            aria-label={description}
             type='button'
             style={{ backgroundColor: color ? color : '#007DF9' }}
             className='flex items-center gap-2 rounded-lg border p-1 font-semibold text-sm'
